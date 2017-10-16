c82fc3970a005864a440eb06424646350961ff03
Mans <manvinder@hiveempire.com>
2017-10-16 17:04:12 +1100

    Added tests

--- test/actionwords.js
+++ test/actionwords.js
@@ -29,7 +29,7 @@ exports.Actionwords = {
   iAmInUserprofilesigninPage: function () {
 
   },
-  iEnterInvalidCredentialsemailAndPassword: function () {
+  iEnterInvalidCredentialsemailAndPassword: function (__free_text) {
 
   },
   errorMessageShouldDisplay: function (__free_text) {
@@ -42,6 +42,48 @@ exports.Actionwords = {
 
   },
   iAmInUserprofilesignupPage: function () {
+
+  },
+  iOnAPageHttpswwwfindercomau: function () {
+
+  },
+  iClickOnRegisterlogin: function () {
+
+  },
+  userLoginModalShouldAppear: function () {
+
+  },
+  iAmInUserLoginModal: function () {
+
+  },
+  thisEmailDoesNotHaveAnAccount: function () {
+
+  },
+  iEnterMyExistingCredentialsInEmailAddressAndPasswordFieldForSignup: function () {
+
+  },
+  iClickOnLogoutButtonInGlobalNavbar: function () {
+
+  },
+  iShouldBeLoggedOutFromUserSytemAndShouldRedirectToUserprofilesignup: function () {
+
+  },
+  iAmOnUserprofilesignin: function () {
+
+  },
+  iClickOnForgetPasswordButton: function () {
+
+  },
+  forgetPasswordEmailSectionShouldDisplay: function () {
+
+  },
+  iAmOnResetMyPasswordPage: function () {
+
+  },
+  iClickOnResetMyPasswordButtonWithoutEnteringEmail: function () {
+
+  },
+  errorMessageShouldLoad: function (__free_text) {
 
   }
 };
